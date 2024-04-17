415f59982596de8c5b29180f360cbbbc881b2d7c
thph27 <118583277+thph27@users.noreply.github.com>
2024-04-17 10:42:26 +0700

    Refactor_Theme_Color

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,4 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { deepOrange, teal, cyan, orange } from '@mui/material/colors'
 
 
 const theme = extendTheme({
@@ -9,18 +8,8 @@ const theme = extendTheme({
   },
 
   colorSchemes: {
-    light: {
-      palette: {
-        primary: teal,
-        secondary: deepOrange
-      }
-    },
-    dark: {
-      palette: {
-        primary: cyan,
-        secondary: orange
-      }
-    }
+    dark: {},
+    light:{}
   },
   components: {
     MuiCssBaseline: {
@@ -31,10 +20,10 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#bdc3c7'
+            backgroundColor: '#dcdde1'
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#00b894'
+            backgroundColor: 'white'
           }
         }
       }
@@ -42,32 +31,31 @@ const theme = extendTheme({
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none'
+          textTransform: 'none',
+          borderWidth:'0.5px',
+          '&:hover': {borderWidth:'0.5px'}
         }
       }
     },
     MuiInputLabel: {
       styleOverrides: {
-        root: ({theme}) => ({
-          color: theme.palette.primary.main,
-          fontSize: '0.875rem'
-        })
+        root: { fontSize: '0.875rem'}
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ({theme}) => ({
-          color: theme.palette.primary.main,
+        root: {
           fontSize: '0.875rem',
-          '.MuiOutlinedInput-notchedOutline':{
-            borderColor: theme.palette.primary.light
+          '& fieldset': {
+            borderWidth: '0.5px !important'
           },
-          '&:hover':{
-            '.MuiOutlinedInput-notchedOutline':{
-              borderColor: theme.palette.primary.light
-            }
+          '&:hover fieldset': {
+            borderWidth: '1px !important'
+          },
+          '&.Mui-focused fieldset': {
+            borderWidth: '1px !important'
           }
-        })
+        }
       }
     }
   }
