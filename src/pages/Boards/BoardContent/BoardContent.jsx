b03fcf439bb227f9b923ce2a4fa75fc63d671a7d
thph27 <118583277+thph27@users.noreply.github.com>
2024-06-07 09:41:03 +0700

    Drag Drop

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,19 +1,64 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext, PointerSensor, MouseSensor, TouchSensor, useSensor, useSensors} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder (board?.columns, board?.columnOrderIds, '_id')
+  // yeu cau chuot di chuyen 10px moi goi event
+  const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  // nhan giu 250ms va dung sai cua cam ung la 500px
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+
+  // const mySensors = useSensors(pointerSensor)
+  const mySensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect (() => {
+    setOrderedColumns(mapOrder (board?.columns, board?.columnOrderIds, '_id'))
+
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    // console.log ('handleDragEnd: ', event)
+    const { active, over} = event
+    if (!over) return
+
+    // neu vi tri sau khi keo tha khac voi vi tri ban dau
+    if (active.id !== over.id) {
+      //Lay vi tri cu tu active
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lay vi tri moi tu over
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+      const dndOrderedColumns = arrayMove (orderedColumns, oldIndex, newIndex)
+      // dung arrayMove cua dnd-kit de sap xep lai mang Columns ban dau
+      // 2 cai console log sau nay de dung xu li goi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log ('dndOrderedColumns: ', dndOrderedColumns)
+      // console.log ('dndOrderedColumnsIds: ', dndOrderedColumnsIds)
+
+      // cap nhat lai state columns ban dau sau khi da keo tha
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
