85d848a98f67a58c78fce15ec63bc61100b6c5a5
thph27 <118583277+thph27@users.noreply.github.com>
2024-06-25 10:05:52 +0700

    Axios

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,25 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect (() => {
+    const boardId = '666d03f3cb2134b5416ea1e2'
+
+    // call API
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board}/>
-      <BoardContent board={mockData?.board}/>
+      <BoardBar board={board}/>
+      <BoardContent board={board}/>
     </Container>
   )
 }
