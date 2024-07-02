e877dc21bd93e7b15364806c9ec70b961db4ae40
thph27 <118583277+thph27@users.noreply.github.com>
2024-07-02 09:41:10 +0700

    Add New Column and Card

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,26 +2,74 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
-import { fetchBoardDetailsAPI } from '~/apis'
+import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis'
+import { generatePlaceholderCard } from '~/utils/formatters'
+import { isEmpty } from 'lodash'
 
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect (() => {
+    // Fix cứng boardId
     const boardId = '667b7c0f2490112fe129ddd9'
-
-    // call API
+    // Call API
     fetchBoardDetailsAPI(boardId).then(board => {
+      // Xu li van de keo tha vao 1 column rong sau khi da co API
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
+      // console.log(board)
       setBoard(board)
     })
   }, [])
+
+  // Goi API tao moi Column va lam lai du lieu State Board
+  const createNewColumn = async (newColumnData) => {
+    const createdColumn = await createNewColumnAPI({
+      ...newColumnData,
+      boardId: board._id
+    })
+    // khi tao 1 column moi chua co card thi can xu li keo tha vao column moi tao chua co card đó
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+    // // Cap nhat state board
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
+  }
+
+  // Goi API tao moi Card va lam lai du lieu State Board
+  const createNewCard = async (newCardData) => {
+    const createdCard = await createNewCardAPI({
+      ...newCardData,
+      boardId: board._id
+    })
+    // Cap nhat state board
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createdCard._id)
+    }
+    setBoard(newBoard)
+  }
+
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board}/>
-      <BoardContent board={mockData.board}/>
+      <BoardBar board={board}/>
+      <BoardContent
+        board={board}
+        createNewColumn = {createNewColumn}
+        createNewCard = {createNewCard}
+      />
     </Container>
   )
 }
