a559000868b54806c8b4c6eee74de679a5a24de5
thph27 <118583277+thph27@users.noreply.github.com>
2024-07-10 14:25:22 +0700

    Deploy node engine 19.x to Vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-38d6.onrender.com'
+export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'https://trello-api-38d6.onrender.com'
